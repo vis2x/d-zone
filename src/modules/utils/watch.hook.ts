99cacdeb39d6c4441407194aee3326012794d1c0
Sagnik Pradhan <markten6321@gmail.com>
2021-01-03 10:50:04 +0530

    🐛 Add cleanup function for watch hook

--- src/modules/utils/watch.hook.ts
+++ src/modules/utils/watch.hook.ts
@@ -5,23 +5,26 @@ import { FunctionType, Nullable } from 'root/typings/util'
  * Watch for change
  *
  * @param fn - Factory function
+ * @param intervalMs - Interval time in milliseconds
  * @returns Previous and current value
  */
-export function useWatch<R>(fn: FunctionType<never[], R>) {
+export function useWatch<R>(fn: FunctionType<never[], R>, intervalMs = 0) {
 	const [value, setValue] = useState<{ prev: Nullable<R>; current: R }>({
 		prev: null,
 		current: fn(),
 	})
 
 	useEffect(() => {
-		setInterval(
+		const interval = setInterval(
 			() =>
 				setValue((value) => ({
 					prev: value.current,
 					current: fn(),
 				})),
-			0
+			intervalMs
 		)
+
+		return () => clearInterval(interval)
 	}, [])
 
 	return value
