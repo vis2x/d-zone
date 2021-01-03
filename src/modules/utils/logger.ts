440b63bd6ace54bf8f6319365a304ed777e1a69c
Sagnik Pradhan <markten6321@gmail.com>
2021-01-03 14:59:39 +0530

    🔊 CSS for browser logger

--- src/modules/utils/logger.ts
+++ src/modules/utils/logger.ts
@@ -1,25 +1,36 @@
-// To keep track of last log
-const consoleStack: BrowserLogger[] = []
-
 export class BrowserLogger {
+	successfullColorRGB = [
+		100 - Math.floor(Math.random() * 80),
+		180 + Math.floor(Math.random() * 175),
+		150 + Math.floor(Math.random() * 105),
+		0.3,
+	]
+
 	constructor(public readonly name: string) {}
 
 	public log(...message: unknown[]) {
-		this.group()
-		console.log(...message)
-		consoleStack.unshift(this)
+		console.log(
+			`%c${this.name}`,
+			`background-color: rgb(${this.successfullColorRGB.join(
+				','
+			)}); padding: 5px 10px;`,
+			...message
+		)
+	}
+
+	public warn(...message: unknown[]) {
+		console.warn(
+			`%c${this.name}`,
+			'background-color: rgba(242, 146, 29, 0.3); padding: 5px 10px;',
+			...message
+		)
 	}
 
 	public error(error: Error) {
-		this.group()
-		console.error(error)
-		consoleStack.unshift(this)
-	}
-
-	private group() {
-		if (consoleStack.shift() !== this) {
-			console.groupEnd()
-			console.group(this.name)
-		}
+		console.error(
+			`%c${this.name}`,
+			'background-color: rgba(242, 29, 29, 0.3); padding: 5px 10px;',
+			error
+		)
 	}
 }
